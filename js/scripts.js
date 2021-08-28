54e78736e77e467e87694abde67372eabf89e3af
mr-jingo <85439298+mr-jingo@users.noreply.github.com>
2021-08-28 18:09:30 +0200

    fixed indentation and fixed jQuery version of crossOut feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 function newItem(){
 //1. Adding a new item to the list of items:
-   let li = $("<li></li>");
-   let inputValue = $("#input").val();
-   li.append(inputValue);
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
 
-   if(inputValue === "") {
-     alert("You must write something!")
-   } else {
-     $("#list").append(li);
-   }
+  if(inputValue === "") {
+    alert("You must write something!")
+  } else {
+    $("#list").append(li);
+  }
 
  //2. Crossing out an item from the list of items:
-   function crossOut() {
- 		li.classList.toggle("strike");
- 	}
+  function crossOut() {
+    li.classList.toggle("strike");
+  }
 
- 	li.on("dblclick", crossOut());
+  li.on("dblclick", function crossOut() {
+		li.toggleClass("strike");
+	});
 
  //3(i). Adding the delete button "X":
    let crossOutButton = document.createElement("crossOutButton");
